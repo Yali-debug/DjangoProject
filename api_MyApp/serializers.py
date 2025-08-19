610b63ac6c1e89dd87fd2e52d8078fc79f34a0b2
Yalinguena <yalinguenabob@gmail.com>
2025-08-19 16:53:03 +0100

    modified serializer

--- api_MyApp/serializers.py
+++ api_MyApp/serializers.py
@@ -8,16 +8,25 @@ class UtilisateurSerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     auteur = UtilisateurSerializer(read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
     
     class Meta:
         model = Article
         fields = '__all__'
         read_only_fields = ('date_publication',)
+    
+    def get_likes_count(self, obj):
+        return obj.like_set.count()
+    
+    def get_comments_count(self, obj):
+        return obj.commentaire_set.count()
 
 class CategorieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categorie
         fields = '__all__'
+
 
 class SousCategorieSerializer(serializers.ModelSerializer):
     categorie = CategorieSerializer(read_only=True)
@@ -26,10 +35,14 @@ class SousCategorieSerializer(serializers.ModelSerializer):
         model = SousCategorie
         fields = '__all__'
 
+
+
 class CentreInteretSerializer(serializers.ModelSerializer):
     class Meta:
         model = CentreInteret
         fields = '__all__'
+
+
 
 class AbonnementSerializer(serializers.ModelSerializer):
     utilisateur = UtilisateurSerializer(read_only=True)
@@ -48,6 +61,8 @@ class ConsulteSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('date_consultation',)
 
+
+
 class LikeSerializer(serializers.ModelSerializer):
     utilisateur = UtilisateurSerializer(read_only=True)
     article = ArticleSerializer(read_only=True)
@@ -60,6 +75,8 @@ class LikeSerializer(serializers.ModelSerializer):
             'utilisateur': {'required': False},
             'article': {'required': False}
         }
+
+
 
 class CommentaireSerializer(serializers.ModelSerializer):
     utilisateur = UtilisateurSerializer(read_only=True)
