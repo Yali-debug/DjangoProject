c62f9a6e30db23dbd0c600e2d2c10f065047b341
Yalinguena <yalinguenabob@gmail.com>
2025-09-06 18:06:49 +0100

    configuration pythonanywhere

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cs7xl$$b2)egq#*+k4rxlc+y-r7fw6^9q=l8*xff+*w=^!*ys(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'YaliDebug.pythonanywhere.com']
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
